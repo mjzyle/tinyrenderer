3ffd3eaad43346bab92d2405eb08fa71b38f5be0
Matt <mjz49@case.edu>
2018-12-11 16:55:01 -0500

    Completed Bresenham tutorial using provided mesh reader files

--- source_code/tinyrenderer/tinyrenderer/main.cpp
+++ source_code/tinyrenderer/tinyrenderer/main.cpp
@@ -1,13 +1,15 @@
 #include <vector>
 #include <cmath>
 #include "tgaimage.h"
-
+#include "geometry.h"
+#include "model.h"
 
 // Mesh variables
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255,   0,   0, 255);
 const int width = 800;
 const int height = 800;
+Model *model = new Model("obj/african_head.obj");
 
 
 // Line drawing method using Bresenham's line algorithm
@@ -53,10 +55,24 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 
 int main(int argc, char** argv) {
 
-	TGAImage image(100, 100, TGAImage::RGB);
+	TGAImage image(width, height, TGAImage::RGB);
 	
 	// Draw mesh
-	/* ADD CUSTOM CODE */
+	for (int i = 0; i < model->nfaces(); i++) {
+		std::vector<int> face = model->face(i);
+
+		for (int j = 0; j < 3; j++) {
+			Vec3f v0 = model->vert(face[j]);
+			Vec3f v1 = model->vert(face[(j + 1) % 3]);
+
+			int x0 = (v0.x + 1.0) * width / 2;
+			int y0 = (v0.y + 1.0) * height / 2;
+			int x1 = (v1.x + 1.0) * width / 2;
+			int y1 = (v1.y + 1.0) * height / 2;
+
+			line(x0, y0, x1, y1, image, white);
+		}
+	}
 
 	image.flip_vertically();
 	image.write_tga_file("output.tga");
